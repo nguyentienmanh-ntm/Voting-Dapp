f24b60b67d3c754220be6e6d3521a6c4a9fa6575
nguyentienmanh-ntm <nguyentienmanh181102@gmail.com>
2023-04-09 10:57:52 +0700

    first commit

---
+++ pages/allowd-voters.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const allowdVoters = () => {
+    return (
+        <div>
+            allowdVoters
+        </div>
+    )
+}
+
+export default allowdVoters
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HOME = () => {
+  return (
+    <div>
+      HOME
+    </div>
+  )
+}
+
+export default HOME
